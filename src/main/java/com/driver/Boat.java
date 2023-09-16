222fa95e0bc967b14a8b7bb409d9a7d939d18efd
shubham <shubham.singhgkp07@gmail.com>
2023-09-16 22:28:31 +0530

    final1

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,23 @@
+package com.driver;
+
+public class Boat implements WaterVehicle
+{
+    String name;
+
+    int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return null;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return 0;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,12 +2,15 @@ package com.driver;
 
 public class F1 extends Car {
 
-    public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
+    public F1(String name, boolean isManual)
+    {
+        super(name, 4, 4, 3, isManual, "f1", 2);
     }
 
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+    public void accelerate(int rate)
+    {
+        int currspeed=this.getCurrentSpeed();
+        int newSpeed = currspeed+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,10 +21,23 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if(newSpeed == 0)
+        {
+            changeGear(1);
             //Stop the car, set gear as 1
         }
-        //for all other cases, change the gear accordingly
+        else if(newSpeed>=1 && newSpeed<=50)
+            changeGear(1);
+        else if(newSpeed>=51 && newSpeed<=100)
+            changeGear(2);
+        else if(newSpeed>=101 && newSpeed<=150)
+            changeGear(3);
+        else if(newSpeed>=151 && newSpeed<=200)
+            changeGear(4);
+        else if(newSpeed>=201 && newSpeed<=250)
+            changeGear(5);
+        else changeGear(6);
+
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
